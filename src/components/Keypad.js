57f4341a8b940131a2722c3e3d77032900898e04
joyce wamocho <joywamo@gmail.com>
2024-11-01 08:10:50 +0300

    adds onchange,onblur and onfocus events

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,9 +1,19 @@
 // Code Keypad Component Here
 
 function Keypad (){
+    const handleChange=(event)=>{
+        console.log("Entering password...")
+
+
+    }
     return (
-        <div></div>
-    )
+        <div>
+            <input 
+            type="password"
+            onChange={handleChange}
+            />
+        </div>
+    );
 }
 
 export default Keypad;
